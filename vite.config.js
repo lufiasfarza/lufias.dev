48ca703f4966385d4201f61eb6c1bbc33e82e330
Lufiasfarza <lufiasfarza@gmail.com>
2025-07-24 15:17:15 +0800

    Deploy Vite to GitHub Pages root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // atau '/nama-repo/' kalau host di github.io/nama-repo
+  base: '/', 
   plugins: [react()],
 });
